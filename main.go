cd6336b3ae5e959a973773cf788a4d0bf8bca209
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-08-16 09:09:22 +0200

    Add version command

--- main.go
+++ main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/frozzare/create-project/project"
 )
 
+var version = "master"
+
 func main() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stderr)
+
 	if len(os.Args) < 2 {
 		log.Fatal("No source url or directory")
 	}
 
 	src := os.Args[1]
+
+	if strings.ToLower(src) == "version" {
+		log.Printf("create-project version %s\n", version)
+		return
+	}
 
 	path, err := os.Getwd()
 	if err != nil {
